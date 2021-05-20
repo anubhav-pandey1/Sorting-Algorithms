9ba64a3dc1123260f92d160cc6eb7815cb57ddaa
Anubhav Pandey <73362441+anubhav-pandey1@users.noreply.github.com>
2021-05-20 21:35:15 +0530

    Create selectionsort.cpp
    Implementation of selection sort in C++

---
+++ selectionsort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionSort(vector<int>& v) {
+    for (int i = 0; i < v.size() - 1; i++) {         // Iterate over the array v.size() - 2 times (last already sorted)
+        int minIndex = i;                            // Set min index equal to the first index in the new unsorted subarray
+        for (int j = i; j < v.size(); j++) {         // Start with the first elem in the unsorted subarray and go to the end
+            if (v[j] < v[minIndex])                  // If any element is less than the current min elem
+                minIndex = j;                        // Update the min index to point to the new min element
+        }                                            // Iteration over the unsorted subarray to find min element gets over
+        iter_swap(v.begin()+minIndex, v.begin()+i);  // Swap the current min element with the first element in unsorted subarray
+    }                                                // Repeat this process once for all v.size() - 1 elements
+}
+
+int main() {
+    // vector<int> v = {1, 1, 1, 1, 2, 2, 2, 2};
+    vector<int> v = {17, 21, 29, 35, 4, 9, 25, 32};
+
+    selectionSort(v);
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
