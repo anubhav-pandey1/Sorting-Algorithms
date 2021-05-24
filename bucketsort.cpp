ba08a960209be21b6ec8143b11afc357d3cf0d98
Anubhav Pandey <73362441+anubhav-pandey1@users.noreply.github.com>
2021-05-24 16:36:26 +0530

    Update bucketsort.cpp with more comments

--- bucketsort.cpp
+++ bucketsort.cpp
@@ -9,22 +9,22 @@ vector<int> bucketSort(vector<int>& v) {             // Using Linked Lists as bu
     int numBuckets = v.size();                       // We can use any number of buckets
     vector<list<int> > B(numBuckets);                // Creating a vector of lists to keep track of buckets
     double max = *max_element(v.begin(), v.end());   // Using a double as using ceil on division of ints has no effect
-    double min = *min_element(v.begin(), v.end());   // Using min and max to estimate the range for the buckets/bins
-    int bucketSize = ceil((max - min)/numBuckets);   // The distance between max and min divided by num of buckets
+    double min = *min_element(v.begin(), v.end());   // Using min and max to estimate the range for the buckets/bins for bucket size
+    int bucketSize = ceil((max - min)/numBuckets);   // The distance between max and min divided by num of buckets gives bucket size
     // int divider = ceil((max+1)/numBuckets);       // We can also use a divider based approach to calculate j-index
 
     for (int i = 0; i < v.size(); i++) {             // Iterating over the array to be sorted for sending elems to buckets
-        int j = floor((v[i] - min)/bucketSize);      // j-index is the specific bucket to which elem is sent
+        int j = floor((v[i] - min)/bucketSize);      // Normalize v[i] to get j-index ie. the specific bucket to which elem is sent
         // int j = floor(v[i]/divider);              // Divider based approach calculates bucket num directly by dividing
         if (j < v.size())                            // IF is required in divider based approach or if max and min are ints
-            B[j].push_back(v[i]);                    // Usually pushing to j-index in the vector of lists works
+            B[j].push_back(v[i]);                    // Usually pushing element to bucket(list) at j-index in the vector of lists works
         else                                         // But j-index for max elems can go out of bounds (> numBuckets) if int is used
             B[numBuckets - 1].push_back(v[i]);       // So for max elem/out of bound elems, put it in the last bucket only
     }
 
     list<int> outList;                               // Create an output list without specifying size (else zeroes initialised)
     for (int j = 0; j < B.size(); j++) {             // Sort all buckets internally stored in the vector B
-        B[j].sort();                                 // std::list::sort() is more approach for sorting lists
+        B[j].sort();                                 // std::list::sort() is a Qsort based approach for sorting lists
         outList.insert(outList.end(), B[j].begin(), B[j].end());
     }                                                // Use list::insert() to add the sorted list to the end of outList
 
